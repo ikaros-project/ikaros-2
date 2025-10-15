1b731925daefeabd50776bbba425920032b703af
Birger Johansson <m@birgerjohansson.com>
2025-10-15 09:56:36 +0200

    Update IKAROS_Serial_BSD.cc
    Adding code for setting the latency timer to 2 ms.

--- Source/Kernel/IKAROS_Serial_BSD.cc
+++ Source/Kernel/IKAROS_Serial_BSD.cc
@@ -171,7 +171,15 @@ Serial::Serial(const char * device_name, unsigned long baud_rate)
 	if (ret)
 		throw SerialException(device_name, "Could not set baud rate", errno);
 #endif
-	
+
+#ifdef SET_LOW_LATENCY_APPLE_DRIVER
+	// Setting latency on apple driver.
+	unsigned long microseconds = 2ULL; // 2ms
+	if ( -1 == ioctl(socket_fd_, IOSSDATALAT, &microseconds) ) {
+		// set latency
+		fprintf(stderr, "\nError calling ioctl(..., IOSSDATALAT, ...)\n");
+	}
+#endif
 	// To make sure that the buffers are empty flush them after 500 ms.
 	Timer t;
 	t.Sleep(500);
